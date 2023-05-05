90dc73160e912a9c8359383d5ebffe185e393fd7
swatidurgekar <swatidurgekar@gmail.com>
2023-05-05 16:55:02 +0530

    style

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import './Button.css';
+import "./Button.css";
 
-const Button = props => {
+const Button = (props) => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button
+      type={props.type}
+      className={!props.isValid ? "button-invalid" : "button"}
+      onClick={props.onClick}
+    >
       {props.children}
     </button>
   );
